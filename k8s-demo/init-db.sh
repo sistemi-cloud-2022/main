57b126be853518bd49213feb869ad5843c22380d
danilo <37438774+khalld@users.noreply.github.com>
2022-07-19 17:14:58 +0200

    Update donor scripts

---
+++ k8s-demo/init-db.sh
@@ -0,0 +1,17 @@
+kubectl apply -f db-secrets.yaml
+
+kubectl apply -f biobank-db-config.yaml
+kubectl apply -f biobank-db.yaml
+
+kubectl apply -f donor-db-config.yaml
+kubectl apply -f donor-db.yaml
+
+kubectl apply -f sample-db-config.yaml
+kubectl apply -f sample-db.yaml
+
+kubectl apply -f shipment-db-config.yaml
+kubectl apply -f shipment-db.yaml
+
+kubectl apply -f sprecsample-db-config.yaml
+kubectl apply -f sprecsample-db.yaml
+
