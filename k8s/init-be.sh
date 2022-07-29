31e7c6052d173dc0ba5d68c130527905fc473b6c
danilo <37438774+khalld@users.noreply.github.com>
2022-07-29 21:19:49 +0200

    Configuration files for BE microservices

--- k8s/init-be.sh
+++ k8s/init-be.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+kubectl apply -f biobank-be-deployment.yaml
+kubectl apply -f biobank-be-service.yaml
+
 kubectl apply -f donor-be-deployment.yaml
 kubectl apply -f donor-be-service.yaml
+
+kubectl apply -f sample-be-deployment.yaml
+kubectl apply -f sample-be-service.yaml
+
+kubectl apply -f shipment-be-deployment.yaml
+kubectl apply -f shipment-be-service.yaml
+
+kubectl apply -f sprecsample-be-deployment.yaml
+kubectl apply -f sprecsample-be-service.yaml
