e95f598004dd898f9e74875dab7f62bd05b0856a
danilo <37438774+khalld@users.noreply.github.com>
2022-07-30 17:10:52 +0200

    update keycloak deploy

--- k8s/aws-be.sh
+++ k8s/aws-be.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# kubectl apply -f biobank-db-config.yaml
-# kubectl apply -f biobank-db.yaml
+kubectl apply -f biobank-be-deployment.yaml -n biobank
+kubectl apply -f biobank-be-service.yaml -n biobank
 
 kubectl apply -f donor-be-deployment.yaml -n donor
 kubectl apply -f donor-be-service.yaml -n donor
 
-# kubectl apply -f sample-db-config.yaml
-# kubectl apply -f sample-db.yaml
+kubectl apply -f sample-be-deployment.yaml -n sample
+kubectl apply -f sample-be-service.yaml -n sample
 
-# kubectl apply -f shipment-db-config.yaml
-# kubectl apply -f shipment-db.yaml
+kubectl apply -f shipment-be-deployment.yaml -n shipment
+kubectl apply -f shipment-be-service.yaml -n shipment
 
-# kubectl apply -f sprecsample-db-config.yaml
-# kubectl apply -f sprecsample-db.yaml
+kubectl apply -f sprecsample-be-deployment.yaml -n sprecsample
+kubectl apply -f sprecsample-be-service.yaml -n sprecsample
 
--- k8s/aws-db.sh
+++ k8s/aws-db.sh
@@ -6,20 +6,18 @@ kubectl create namespace sample
 kubectl create namespace shipment
 kubectl create namespace sprecsample
 
-# TODO Aggiungi per completezza -n namespace
+kubectl apply -f biobank-db-config.yaml -n biobank
+kubectl apply -f biobank-db.yaml -n biobank
 
-kubectl apply -f biobank-db-config.yaml
-kubectl apply -f biobank-db.yaml
+kubectl apply -f donor-db-config.yaml -n donor
+kubectl apply -f donor-db.yaml -n donor
 
-kubectl apply -f donor-db-config.yaml
-kubectl apply -f donor-db.yaml
+kubectl apply -f sample-db-config.yaml -n sample
+kubectl apply -f sample-db.yaml -n sample
 
-kubectl apply -f sample-db-config.yaml
-kubectl apply -f sample-db.yaml
+kubectl apply -f shipment-db-config.yaml -n shipment
+kubectl apply -f shipment-db.yaml -n shipment
 
-kubectl apply -f shipment-db-config.yaml
-kubectl apply -f shipment-db.yaml
-
-kubectl apply -f sprecsample-db-config.yaml
-kubectl apply -f sprecsample-db.yaml
+kubectl apply -f sprecsample-db-config.yaml -n sprecsample
+kubectl apply -f sprecsample-db.yaml -n sprecsample
 
