86e38ba7cf24a207f67e318befbb5653ab7721d5
alessioliotta95 <55488696+alessioliotta95@users.noreply.github.com>
2022-08-01 19:48:15 +0200

    feat: kubernetes and keycloak config done

--- k8s/aws-be.sh
+++ k8s/aws-be.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-kubectl apply -f biobank-be-deployment.yaml -n biobank
-kubectl apply -f biobank-be-service.yaml -n biobank
+# kubectl apply -f biobank-be-deployment.yaml
+# kubectl apply -f biobank-be-service.yaml 
 
-kubectl apply -f donor-be-deployment.yaml -n donor
-kubectl apply -f donor-be-service.yaml -n donor
+kubectl apply -f donor-be-deployment.yaml 
+kubectl apply -f donor-be-service.yaml
 
-kubectl apply -f sample-be-deployment.yaml -n sample
-kubectl apply -f sample-be-service.yaml -n sample
+# kubectl apply -f sample-be-deployment.yaml
+# kubectl apply -f sample-be-service.yaml
 
-kubectl apply -f shipment-be-deployment.yaml -n shipment
-kubectl apply -f shipment-be-service.yaml -n shipment
+# kubectl apply -f shipment-be-deployment.yaml
+# kubectl apply -f shipment-be-service.yaml
 
-kubectl apply -f sprecsample-be-deployment.yaml -n sprecsample
-kubectl apply -f sprecsample-be-service.yaml -n sprecsample
+# kubectl apply -f sprecsample-be-deployment.yaml
+# kubectl apply -f sprecsample-be-service.yaml
 
