10c181220284072e86627f69a316af2a78a0b537
alessioliotta95 <55488696+alessioliotta95@users.noreply.github.com>
2022-08-02 20:09:21 +0200

    Update restore-k8s.sh

--- k8s/restore-k8s.sh
+++ k8s/restore-k8s.sh
@@ -6,3 +6,7 @@ kubectl delete deployment --all -n biobanksprec
 kubectl delete configmap --all -n biobanksprec
 kubectl delete secret --all -n biobanksprec
 kubectl delete ingress --all -n biobanksprec
+
+#Delete volumes
+kubectl delete pvc init-script -n biobanksprec
+kubectl delete pvc mysql-data-pvc -n biobanksprec
