a248a734e2710c465bccb49a5e3bf8912335a293
danilo <37438774+khalld@users.noreply.github.com>
2022-08-02 23:04:22 +0200

    MAIN-FIX: Deployed test 1 on aws. Read description
    - Update restore-k8.sh
    - FIX: Now BE can talk with db adding type: NodePort, with LoadBalancer can also reach the db from external but is not necessary
    - ERROR: sample-be-deployment load the docker image of local docker not the registry
    - Keycloak still not working

--- k8s/aws-be.sh
+++ k8s/aws-be.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-kubectl apply -f keycloak.yml
+# kubectl apply -f keycloak.yml
 
-kubectl apply -f biobank-be-deployment.yaml
-kubectl apply -f biobank-be-service.yaml 
+# kubectl apply -f biobank-be-deployment.yaml
+# kubectl apply -f biobank-be-service.yaml 
 
 kubectl apply -f donor-be-deployment.yaml 
 kubectl apply -f donor-be-service.yaml
 
-kubectl apply -f sample-be-deployment.yaml
-kubectl apply -f sample-be-service.yaml
+# kubectl apply -f sample-be-deployment.yaml
+# kubectl apply -f sample-be-service.yaml
 
-kubectl apply -f shipment-be-deployment.yaml
-kubectl apply -f shipment-be-service.yaml
+# kubectl apply -f shipment-be-deployment.yaml
+# kubectl apply -f shipment-be-service.yaml
 
-kubectl apply -f sprecsample-be-deployment.yaml
-kubectl apply -f sprecsample-be-service.yaml
+# kubectl apply -f sprecsample-be-deployment.yaml
+# kubectl apply -f sprecsample-be-service.yaml
 
+# kubectl config use-context iam-root-account@biobank-sprec-v1.eu-central-1.eksctl.io
+# config use-context docker-desktop
